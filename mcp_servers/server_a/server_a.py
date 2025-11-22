51e21f02e14a8d9cba0b8678d91a40281b157251
Gabriel Avelar <117688731+gabrielvavelar@users.noreply.github.com>
2025-11-22 20:13:51 -0300

    Add files via upload

---
+++ mcp_servers/server_a/server_a.py
@@ -0,0 +1,44 @@
+from mcp.server.fastmcp import FastMCP
+from dotenv import load_dotenv
+import google.genai as genai
+import os
+from utils import *
+
+mcp = FastMCP("Image MCP")
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+IMAGE_MODEL = "gemini-2.5-flash-image"
+
+@mcp.tool()
+async def create_image(input: str) -> str:
+    """
+    Create image
+    
+    """
+    try:
+        response = client.models.generate_content(
+            model=IMAGE_MODEL,
+            contents=input
+        )
+
+        for part in getattr(response, "parts", []):
+            inline_data = getattr(part, "inline_data", None)
+            if inline_data and getattr(inline_data, "data", None):
+                img = inline_data.data
+
+                file_path = save_image_locally(img)
+
+                image_url = create_img_url(file_path)
+                
+                return image_url
+
+    except Exception as e:
+        print(f"Erro ao gerar imagem: {e}")
+
+    return None
+
+if __name__ == "__main__":
+    mcp.run()
---
+++ mcp_servers/server_a/utils.py
@@ -0,0 +1,41 @@
+import cloudinary
+import cloudinary.uploader
+import os
+from dotenv import load_dotenv
+import uuid
+from pathlib import Path
+
+load_dotenv()
+
+cloudinary.config( 
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    secure=True
+)
+
+# Cria a pasta storage se não existir
+
+BASE_DIR = Path(__file__).resolve().parent.parent   # sobe de mcp_servers para src
+STORAGE_DIR = BASE_DIR / "storage"
+STORAGE_DIR.mkdir(exist_ok=True)
+
+
+def save_image_locally(img: bytes) -> str:
+    file_name = f"{uuid.uuid4()}.jpg"
+    file_path = STORAGE_DIR / file_name
+
+    with open(file_path, "wb") as f:
+        f.write(img)
+
+    return str(file_path)
+
+
+def create_img_url(file_path: str) -> str:
+    # Cloudinary precisa do caminho completo do arquivo
+    result = cloudinary.uploader.upload(
+        file_path,
+        resource_type="image"
+    )
+
+    return result["secure_url"]
